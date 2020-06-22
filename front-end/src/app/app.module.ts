c6fe0d00d8e18f3b45e56c96f78efeda6c873c5d
Riccardo Manca <Mayonakano95@hotmail.it>
2020-06-22 10:19:00 +0200

    Persone Push

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersoneComponent } from './persone/persone.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersoneComponent
   ],
   imports: [
     BrowserModule,
